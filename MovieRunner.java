fd3275279ed32fbcb83f69fbad23201f1e126bdc
priyadarshinika05-coder <priyadarshinika05@gmail.com>
2026-04-05 11:54:06 +0530

    constructor chaining

---
+++ MovieRunner.java
@@ -0,0 +1,10 @@
+class MovieRunner{
+
+    public static void main(String[] args) {
+
+        Movie m1=new Movie();
+	    Movie m2=new Movie();
+		Movie m3=new Movie();
+        Movie m4=new Movie();
+    }
+}
---
+++ UberRunner.java
@@ -0,0 +1,10 @@
+class UberRunner{
+
+    public static void main(String[] args) {
+
+        Uber u1=new Uber();
+		Uber u2=new Uber();
+		Uber u3=new Uber();
+        Uber u4=new Uber();
+    }
+}
